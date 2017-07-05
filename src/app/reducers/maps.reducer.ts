07e1ca4816dfc84c6c67ba96f52b03753fa78d58
hxhung <hung.xuan@kellydeli.com>
2017-07-05 16:00:23 +0200

    chore(): places state is now handle multiple rooms

--- src/app/reducers/maps.reducer.ts
+++ src/app/reducers/maps.reducer.ts
@@ -2,24 +2,39 @@ import { Observable } from 'rxjs/Observable';
 import { PlacesActionTypes as types, LabelAction } from '../actions/maps.action';
 
 const initialState = {
-  data: [],
+  data: {},
   lastAction: null,
 }
+const room = (state, action) => {
+  switch (action.type) {
+    case types.ADD_PLACE:
+      return {
+        ...action.payload
+      };
+    default:
+      return state;
+  }
+};
 
 export function reducer(state = initialState, action) {
   let payload = action.payload;
   switch (action.type) {
     case types.ADD_PLACE:
-      console.log(`[Place]Add place`, state, action);
-      let newData = state.data.map(place => ({ ...place }));
-      newData.push(payload);
-      return Object.assign({}, state, {
+      const roomId = `ROOM_1`;
+      const placeName = payload.name;
+      const roomplaces = roomId in state.data ? { ...state.data[roomId] } : {};
+      let roomsWithplaces = { ...state.data };
+      if (placeName in roomplaces) {
+        return state;
+      }
+      roomplaces[placeName] = room(undefined, action);
+      roomsWithplaces[roomId] = { ...roomplaces };
+      return {
+        ...state,
         data: {
-          ...newData
+          ...roomsWithplaces
         }
-      });
-    default:
-      return state;
+      };
   }
 }
 
