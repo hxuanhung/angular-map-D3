7a0139b40203840ae02b64a96ecc8e4db27da5e3
hxhung <hung.xuan@kellydeli.com>
2017-07-06 18:41:49 +0200

    feat(): add d3 layer with circles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ import { store as reduxStore } from '../store';
 import { addPlaceAction, removePlaceAction } from '../actions/maps.action';
 import { MapService } from '../services/map.service';
 import * as L from 'leaflet';
+declare let d3: any;
 interface MapsStore {
   data: {}
 }
@@ -46,8 +47,8 @@ export class HomeComponent implements OnInit {
   public ngOnInit() {
     let map = L.map("map", {
       zoomControl: false,
-      center: L.latLng(40.731253, -73.996139),
-      zoom: 12,
+      center: L.latLng(22.30692675357551, 114.18306345846304),
+      zoom: 4,
       minZoom: 4,
       maxZoom: 19,
       layers: [this.mapService.baseMaps.OpenStreetMap]
@@ -56,6 +57,82 @@ export class HomeComponent implements OnInit {
     L.control.zoom({ position: "topright" }).addTo(map);
     L.control.layers(this.mapService.baseMaps).addTo(map);
     L.control.scale().addTo(map);
+    // http://bl.ocks.org/d3noob/9211665
+    //http://plnkr.co/edit/Lqme891ee8rwDiXktY5L?p=preview
+    //https://bost.ocks.org/mike/leaflet/
+    //https://tools.ietf.org/html/rfc7946#section-3.1.3
+    let svg = d3.select(map.getPanes().overlayPane).append("svg"),
+      g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+    // d3.json("https://api.myjson.com/bins/113nlr", function (geoShape) {
+    d3.json("https://api.myjson.com/bins/7ieu7", function (error, myPoints) {
+      if (error) throw error;
+
+      var transform = d3.geo.transform({
+        point: projectPoint
+      }),
+        path = d3.geo.path().projection(transform);
+
+      var bounds = path.bounds(myPoints),
+        topLeft = bounds[0],
+        bottomRight = bounds[1];
+
+      myPoints.features.forEach(function (d) {
+        d.LatLng = new L.LatLng(d.geometry.coordinates[1],
+          d.geometry.coordinates[0]);
+      });
+
+      var circles = g.selectAll("circle")
+        .data(myPoints.features)
+        .enter()
+        .append("circle")
+        .attr("r", 10)
+        .style("fill", "red")
+        .attr("fill-opacity", 0.5);
+
+      // Use Leaflet to implement a D3 geometric transformation.
+      function projectPoint(x, y) {
+        var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+        this.stream.point(point.x, point.y);
+      }
+
+      function update() {
+        circles.attr("cx", function (d) {
+          return map.latLngToLayerPoint(d.LatLng).x;
+        });
+        circles.attr("cy", function (d) {
+          return map.latLngToLayerPoint(d.LatLng).y;
+        });
+        svg.attr("width", bottomRight[0] - topLeft[0])
+          .attr("height", bottomRight[1] - topLeft[1])
+          .style("left", topLeft[0] + "px")
+          .style("top", topLeft[1] + "px");
+
+        g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+      }
+
+      map.on("viewreset", update);
+      update();
+
+      var LeafIcon = L.Icon.extend({
+        options: {
+          iconSize: [13, 22],
+          iconAnchor: [6, 16],
+          popupAnchor: [-3, -76]
+        }
+      });
+      var fIcon = new LeafIcon({ iconUrl: 'puce_f.png' });
+      myPoints.features.forEach(function (d) {
+        d.LatLng = new L.LatLng(d.geometry.coordinates[1],
+          d.geometry.coordinates[0]);
+        L.marker([d.geometry.coordinates[1], d.geometry.coordinates[0]], { icon: fIcon }).bindPopup(`${d.properties.name}`).addTo(map);
+      });
+
+    })
+  }
+
+  public addCitiesPins(map) {
+
   }
 
   public getAddress(place: Object) {
