1bea80f89c1ef00c4dbec895c5f794b6271228b5
hxhung <hung.xuan@kellydeli.com>
2017-07-07 12:21:06 +0200

    feat(): test D3SvgOverlay

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,9 +9,10 @@ import { Store } from '@ngrx/store';
 import { store as reduxStore } from '../store';
 import { addPlaceAction, removePlaceAction } from '../actions/maps.action';
 import { MapService } from '../services/map.service';
-import * as L from 'leaflet';
-import {DATA as myPoints} from './data';
+// import * as L from 'leaflet';
+import { DATA as myPoints } from './data';
 declare let d3: any;
+declare let L: any;
 interface MapsStore {
   data: {}
 }
@@ -46,8 +47,8 @@ export class HomeComponent implements OnInit {
   public ngOnInit() {
     let map = L.map("map", {
       zoomControl: false,
-      center: L.latLng(22.30692675357551, 114.18306345846304),
-      zoom: 4,
+      center: L.latLng(46.81509864599243, 8.3221435546875),
+      zoom: 8,
       minZoom: 4,
       maxZoom: 19,
       layers: [this.mapService.baseMaps.OpenStreetMap]
@@ -61,97 +62,57 @@ export class HomeComponent implements OnInit {
     //https://bost.ocks.org/mike/leaflet/
     //https://tools.ietf.org/html/rfc7946#section-3.1.3
     // http://bl.ocks.org/matt-mcdaniel/47033dfacfd7553489ea D3 + Leaflet with Auto Fit to Bounds
-    let svg = d3.select(map.getPanes().overlayPane).append("svg"),
-      g = svg.append("g").attr("class", "leaflet-zoom-hide");
+    var cities = [];
+    var data = {
+      test: [{
+        name: "Meyrin",
+        geometry: {
+          location: {
+            lat: 46.2328413,
+            lng: 6.0817791
+          },
+        }
+      }]
+    };
+    var citiesOverlay = L.d3SvgOverlay(function (sel, proj) {
+      sel.selectAll('.mark').data(cities)
+        .enter()
+        .append('image')
+        .attr('class', 'mark')
+        .attr('x', function (d) {
+          return proj.latLngToLayerPoint(d.latLng).x;
+        })
+        .attr('y', function (d) {
+          return proj.latLngToLayerPoint(d.latLng).y;
+        })
+        .attr('width', 20)
+        .attr('height', 20)
+        .attr("xlink:href", 'https://cdn3.iconfinder.com/data/icons/softwaredemo/PNG/24x24/DrawingPin1_Blue.png');
 
-    // d3.json("https://api.myjson.com/bins/113nlr", function (geoShape) {
-    // d3.json("https://api.myjson.com/bins/7ieu7", function (error, myPoints) {
-    //   if (error) throw error;
-    var transform = d3.geo.transform({
-      point: projectPoint
-    }),
-      path = d3.geo.path().projection(transform);
-    var bounds = path.bounds(myPoints),
-    topLeft = bounds[0],
-    bottomRight = bounds[1];
-
-  myPoints.features.forEach(function (d) {
-    d[`LatLng`] = new L.LatLng(d.geometry.coordinates[1],
-      d.geometry.coordinates[0]);
-  });
-
-  var circles = g.selectAll(".mark")
-    .data(myPoints.features)
-    .enter()
-    .append("image")
-    .attr('class', 'mark')
-    .attr('width', 20)
-    .attr('height', 20)
-    .attr("xlink:href", 'https://cdn3.iconfinder.com/data/icons/softwaredemo/PNG/24x24/DrawingPin1_Blue.png');
-
-  var texts = g.selectAll(".place-label")
-    .data(myPoints.features)
-    .enter()
-    .append("text")
-    .attr("class", "place-label")
-    .attr("dy", "2.35em")
-    .text(function (d) { return d.properties.name; });
-  // var circles = g.selectAll("circle")
-  //   .data(myPoints.features)
-  //   .enter()
-  //   .append("circle")
-  //   .attr("r", 10)
-  //   .style("fill", "red")
-  //   .attr("fill-opacity", 0.5);
-
-  // Use Leaflet to implement a D3 geometric transformation.
-  function projectPoint(x, y) {
-    var point = map.latLngToLayerPoint(new L.LatLng(y, x));
-    this.stream.point(point.x, point.y);
-  }
-
-  function update() {
-    circles.attr("x", function (d) {
-      return map.latLngToLayerPoint(d.LatLng).x;
+      sel.selectAll(".place-label")
+        .data(cities)
+        .enter()
+        .append("text")
+        .attr('x', function (d) {
+          return proj.latLngToLayerPoint(d.latLng).x;
+        })
+        .attr('y', function (d) {
+          return proj.latLngToLayerPoint(d.latLng).y;
+        })
+        .attr("class", "place-label")
+        .attr("dy", "2.35em")
+        .text(function (d) {
+          return d.name;
+        });
     });
-    circles.attr("y", function (d) {
-      return map.latLngToLayerPoint(d.LatLng).y;
+
+    cities = data.test.map(function (d) {
+      d[`latLng`] = [d.geometry.location.lat, d.geometry.location.lng];
+      return d;
     });
-    texts.attr("x", function (d) {
-      return map.latLngToLayerPoint(d.LatLng).x;
-    });
-    texts.attr("y", function (d) {
-      return map.latLngToLayerPoint(d.LatLng).y;
-    });
-    svg.attr("width", bottomRight[0] - topLeft[0])
-      .attr("height", bottomRight[1] - topLeft[1])
-      .style("left", topLeft[0] + "px")
-      .style("top", topLeft[1] + "px");
-
-    g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
-  }
-
-  map.on("viewreset", update);
-  update();
+    citiesOverlay.addTo(map);
 
 
-
-
-      // var LeafIcon = L.Icon.extend({
-      //   options: {
-      //     iconSize: [13, 22],
-      //     iconAnchor: [6, 16],
-      //     popupAnchor: [-3, -76]
-      //   }
-      // });
-      // var fIcon = new LeafIcon({ iconUrl: 'puce_f.png' });
-      // myPoints.features.forEach(function (d) {
-      //   d.LatLng = new L.LatLng(d.geometry.coordinates[1],
-      //     d.geometry.coordinates[0]);
-      //   L.marker([d.geometry.coordinates[1], d.geometry.coordinates[0]], { icon: fIcon }).bindPopup(`${d.properties.name}`).addTo(map);
-      // });
-
-    // })
     this.places$ = this.store.let(reduxStore.places.getPlaces).subscribe(places => {
       const roomPlaces = places[`ROOM_1`];
       this.places = roomPlaces != null ? Object.keys(roomPlaces).map(key => roomPlaces[key]) : [];
